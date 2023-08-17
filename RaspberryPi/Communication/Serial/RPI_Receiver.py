cbee81506ce944580a0589d7c66ba1b4afc6e127
Anjum Sohail <itdanjum@gmail.com>
2023-08-17 05:40:32 -0700

    Update RPI_Receiver.py

--- RaspberryPi/Communication/Serial/RPI_Receiver.py
+++ RaspberryPi/Communication/Serial/RPI_Receiver.py
@@ -1,7 +1,10 @@
 import serial
 
-# Replace '/dev/ttyS0' with the appropriate serial port of your Raspberry Pi
-ser = serial.Serial('COM9', 9600)
+# Replace '/dev/ttyS0' for UART0 with the appropriate serial port of your Raspberry Pi
+# GPIO14 (TXD) - Pin 8    GPIO15 (RXD) - Pin 10
+# device file for SPI could be like /dev/spidevX,Y : where X represents the bus number and Y represents the chip select (CS) number: i.e /dev/spidev0.0
+# Device File for I2C could be like /dev/i2c-X:  where X is the bus number, so it could be /dev/i2c-1
+ser = serial.Serial('/dev/ttyS0', 9600)
 
 try:
     while True:

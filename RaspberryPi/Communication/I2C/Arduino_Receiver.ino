dab6ecb54beb94569cdb07ac8a415e350a72b94b
Anjum Sohail <itdanjum@gmail.com>
2023-08-17 06:10:34 -0700

    Update Arduino_Receiver.ino

--- RaspberryPi/Communication/I2C/Arduino_Receiver.ino
+++ RaspberryPi/Communication/I2C/Arduino_Receiver.ino
@@ -1,4 +1,6 @@
 #include <Wire.h>
+# Arduino Pins A4 (SDA), A5 (SCL)
+# RaspberryPi Pins SDA: GPIO2 (BCM pin 2)  SCL: GPIO3 (BCM pin 3)
 
 void setup() {
   Wire.begin(0x08); // I2C address of the Arduino slave

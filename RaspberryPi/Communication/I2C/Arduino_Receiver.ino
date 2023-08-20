91f8f9b5292e99a4d576a07404bef3d59c0afa82
Anjum Sohail <itdanjum@gmail.com>
2023-08-20 14:00:29 +0500

    Update Arduino_Receiver.ino

--- RaspberryPi/Communication/I2C/Arduino_Receiver.ino
+++ RaspberryPi/Communication/I2C/Arduino_Receiver.ino
@@ -1,20 +1,31 @@
+//This code is slave and it send and receive the data
+// use sudo i2cdetect -y 1 
 #include <Wire.h>
-# Arduino Pins A4 (SDA), A5 (SCL)
-# RaspberryPi Pins SDA: GPIO2 (BCM pin 2)  SCL: GPIO3 (BCM pin 3)
+#define SDA_PIN D2       //Declare SCL Pin on NodeMCU 
+#define SCL_PIN D1         //Declare SDA Pin on NodeMCU
+#define ADDRESS 5
 
 void setup() {
-  Wire.begin(0x08); // I2C address of the Arduino slave
-  Wire.onReceive(receiveEvent);
-  Serial.begin(9600);
+ Wire.begin(SCL_PIN, SDA_PIN,ADDRESS); //start I2C communication on address 8
+ Wire.onReceive(receiveEvent);   //register receive event
+ Wire.onRequest(requestEvent);   //register request event
+ Serial.begin(9600);             //start serial for debug
 }
 
 void loop() {
-  delay(100);
+ delay(1000);
 }
 
-void receiveEvent(int bytes) {
-  while (Wire.available()) {
-    char c = Wire.read();
-    Serial.print(c);
+// function that executes whenever data is received from master
+void receiveEvent(int howMany) {
+ while (0 <Wire.available()) {
+    char c = Wire.read();      //receive byte as a character
+    Serial.print(c);           // print the character 
   }
+ Serial.println();             // to newline
+}
+
+// function that executes whenever data is requested from master
+void requestEvent() {
+ Wire.write("HELLO_PICO");  //send string on request
 }
